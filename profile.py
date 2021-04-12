1e2e694065662779509909c07b389a22431404cf
Isaak G <ike091@gmail.com>
2021-04-12 15:35:44 -0600

    Add public IP count parameter

--- profile.py
+++ profile.py
@@ -9,28 +9,35 @@ import geni.rspec.pg as pg
 import geni.rspec.emulab as emulab
 import geni.rspec.igext as igext
 
-# define some constants
+# Define OS image
 CENTOS7_IMG = 'urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD'
 
-# the number of extra public ip addresses to allocate
-NUM_IP_ADDRESSES = 2
-
-# create a portal context, needed to define parameters
+# Create a portal context, needed to define parameters
 pc = portal.Context()
 
-# create a Request object to start building RSpec
+# Create a Request object to start building RSpec
 request = pc.makeRequestRSpec()
 
-# create a node
+# Create some user-configurable parameters
+pc.defineParameter('public_ip_count', 'The number of additional public IPs to allocate', portal.ParameterType.INTEGER, 2)
+
+params = pc.bindParameters()
+
+# Validate parameters
+if params.public_ip_count < 1:
+    pc.reportError(portal.ParameterError('You must allocate at least 1 additional public ip.', ['public_ip_count']))
+
+# Create a node
 node = request.RawPC('node1')
 
+# Set node image
 node.disk_image = CENTOS7_IMG
 
-# request a pool of dynamic publically routable ip addresses - pool name cannot contain underscores - hidden bug
-addressPool = igext.AddressPool('addressPool', NUM_IP_ADDRESSES)
+# Request a pool of dynamic publically routable ip addresses - pool name cannot contain underscores - hidden bug
+addressPool = igext.AddressPool('addressPool', params.public_ip_count)
 addressPool.component_manager_id = ('urn:publicid:IDN+utah.cloudlab.us+authority+cm')
 request.addResource(addressPool)
 
-# output RSpec
+# Output RSpec
 pc.printRequestRSpec(request)
 
