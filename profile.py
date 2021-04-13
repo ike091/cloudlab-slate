67b17f938daafb591aea466dc34436d38f1f4b9c
Isaak G <ike091@gmail.com>
2021-04-13 09:40:15 -0600

    Remove specific site public IP info

--- profile.py
+++ profile.py
@@ -36,7 +36,6 @@ node.disk_image = CENTOS7_IMG
 
 # Request a pool of dynamic publically routable ip addresses - pool name cannot contain underscores - hidden bug
 addressPool = igext.AddressPool('addressPool', int(params.public_ip_count))
-addressPool.component_manager_id = ('urn:publicid:IDN+utah.cloudlab.us+authority+cm')
 request.addResource(addressPool)
 
 # Output RSpec
